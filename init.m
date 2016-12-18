ea3609d203f95c7c3199f969f2baffc2016e4018
motttey <motitago@gmail.com>
2016-12-18 22:21:52 +0900

    add init.m

---
+++ init.m
@@ -0,0 +1,16 @@
+%init vals
+Resize_Width = 64;
+Resize_Height = 64;
+Face_Class_Num = 20;
+Individual_Face_Num = 35;
+DB_MAX = 695;
+QUERY_MAX = 161;
+
+%init path
+db_path = '/Users/takamirei/Google ドライブ/情報通信プロジェクト実験B班/dataset/DB/jpeg/';
+db_path_crop = '/Users/takamirei/Google ドライブ/情報通信プロジェクト実験B班/dataset/Query/crop_hist/';
+
+query_path = '/Users/takamirei/Google ドライブ/情報通信プロジェクト実験B班/dataset/Query/jpeg/';
+query_path_regex = '/Users/takamirei/Google ドライブ/情報通信プロジェクト実験B班/dataset/Query/jpeg/*.jpg';
+query_path_l = '/Users/takamirei/Google ドライブ/情報通信プロジェクト実験B班/dataset/Query/jpeg/*jpg';
+query_path_crop = '/Users/takamirei/Google ドライブ/情報通信プロジェクト実験B班/dataset/Query/crop_hist/';
